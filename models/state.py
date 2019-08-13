fbd16bb059a0400d53b793c0dd2a13bfaadc509b
Marc Cavigli <mcavigli@gmail.com>
2019-08-13 20:16:53 +0000

    Changed to declarative functionality

--- models/state.py
+++ models/state.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)
+
+    @property
+    def cities(self):
+        """returns list of City instances with state_id"""
+        return {k: v for k, v in storage.all().items()
+                if v.state_id == self.id}
