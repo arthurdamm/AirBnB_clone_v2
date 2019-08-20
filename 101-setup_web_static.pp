6bfab462967a0870d756d28819b422efaa53e938
Arthur Damm <indigoarthur@gmail.com>
2019-08-20 03:13:19 +0000

    add 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -19,6 +19,5 @@ exec { '/usr/bin/env apt-get -y update' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
 -> exec { '/usr/bin/env echo "Hello Holberton School!" > /data/web_static/releases/test/index.html' : }
 -> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
--> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data/' : }
 -> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env service nginx restart' : }
