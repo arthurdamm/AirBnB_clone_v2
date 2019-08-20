ca2dc5b52eac0d32670ff95a9503e6d9225689cc
Arthur Damm <indigoarthur@gmail.com>
2019-08-20 03:55:14 +0000

    add 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,28 +1,15 @@
-# Installs nginx n stuff
+# Installs nginx and stuff like that
 exec { '/usr/bin/env apt-get -y update' : }
--> package { 'nginx':
-  ensure => installed,
-}
--> file { '/var/www/html/index.html' :
-  content => 'Holberton School!',
-}
--> file_line { 'add header' :
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "\tadd_header X-Served-By ${hostname};",
-  after  => 'listen \[::\]:80 default_server;',
-}
--> file_line { 'add location' :
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "\tlocation /hbnb_static { alias /data/web_static/current/; }",
-  after  => "\tadd_header X-Served-By ${hostname};",
-}
--> service { 'nginx':
-  ensure => running,
-}
+-> exec { '/usr/bin/env apt-get -y install nginx' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env # echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html' : }
+-> exec { '/usr/bin/env service nginx start' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
 -> exec { '/usr/bin/env echo "Hello Holberton School!" > /data/web_static/releases/test/index.html' : }
 -> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
+-> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env service nginx restart' : }
+-> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data/' : }
