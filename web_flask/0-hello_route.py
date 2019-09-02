8d9d257d85b3ec1663729b0035d8e7443ce3c910
Arthur Damm <indigoarthur@gmail.com>
2019-09-02 18:38:14 +0000

    add 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,11 @@
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.run("0.0.0.0", 5000)
 
 
 @app.route('/')
 def index():
     """Route index"""
-    print("Hello HBNB!")
+    return "Hello HBNB!"
+
+app.run("0.0.0.0", 5000)
